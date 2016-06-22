44b1e32206e557a9a50ecaffe8afea5ddf2d622f
Jonathan Barbeau <jb@mambomambo.ca>
2016-06-22 14:48:23 -0400

    Finition de la liste interet/mise en prod

--- wp-content/themes/bia/templates/contact-form.php
+++ wp-content/themes/bia/templates/contact-form.php
@@ -61,7 +61,7 @@
           <div class="response" id="mce-success-response" style="display:none"></div>
         </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
         <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_5651c764d0b83a317b47e6201_44a1134806" tabindex="-1" value=""></div>
-        <div class="clear"><span class="triangleRouge"><input class="submit" type="submit" value="S'inscrire" name="subscribe" id="mc-embedded-subscribe" class="button"></span><label for="mc-embedded-subscribe"></label></div>
+        <div class="clear"><span class="triangleRouge"><input class="submit" type="submit" value="S'inscrire" name="subscribe" id="mc-embedded-subscribe" class="button"></span></div>
       </div>
     </form>
   </div>
