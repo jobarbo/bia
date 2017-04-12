2e18aa2bf20306be588aa8ae587a5e6079835272
Jonathan Martin <jm@mambomambo.ca>
2017-04-12 11:18:10 -0400

    Added membre-club-bia related notice string in cart page

--- wp-content/themes/bia/woocommerce/cart/cart.php
+++ wp-content/themes/bia/woocommerce/cart/cart.php
@@ -135,6 +135,8 @@ do_action( 'woocommerce_before_cart' ); ?>
 						<label for="coupon_code"><?php _e( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <span class="triangleRouge"><input type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply Coupon', 'woocommerce' ); ?>" /></span>
 
 						<?php do_action( 'woocommerce_cart_coupon' ); ?>
+
+						<p class="membre-club-bia-cart-notice"><small>*Si vous êtes membre du Club Bia, cochez la case correspondante lors de la prochaine étape.</small></p>
 					</div>
 				<?php } ?>
 
