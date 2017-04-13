b85d3aa265d18314ee82115149212adedb353c3a
Jonathan Martin <jm@mambomambo.ca>
2017-04-13 12:27:27 -0400

    Updated YoastSEO 4.6

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInita2231c82309a04c3b7d7679bf9a57481::getLoader();
+return ComposerAutoloaderInitfcd4149ae779ace07774f6f7392b5843::getLoader();
